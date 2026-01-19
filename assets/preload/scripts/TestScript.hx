3fb3b89da65b13c53d193d9889ff6e02a80e5ab6
Macohi <sinconsistencia@gmail.com>
2026-01-19 17:41:51 -0600

    scriptsLoaded

--- assets/preload/scripts/TestScript.hx
+++ assets/preload/scripts/TestScript.hx
@@ -1,4 +1,4 @@
-function miscScriptsLoaded()
+function scriptsLoaded()
 {
     trace('Hey');
 }
--- example_mods/introMod/scripts/TestScript.hx
+++ example_mods/introMod/scripts/TestScript.hx
@@ -1,4 +1,4 @@
-function miscScriptsLoaded()
+function scriptsLoaded()
 {
     trace('YO WHATUP BEEEEEEEEEAAAAAACCCCCHHHHHHHHH??????????');
 }

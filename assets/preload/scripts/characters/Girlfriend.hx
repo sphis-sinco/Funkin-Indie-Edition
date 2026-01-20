a8a8b4e54792344f336a3d138abd01cb522053cd
Macohi <sinconsistencia@gmail.com>
2026-01-19 19:48:44 -0600

    keep the fix

--- assets/preload/scripts/characters/Girlfriend.hx
+++ assets/preload/scripts/characters/Girlfriend.hx
@@ -12,7 +12,7 @@ function update(character:Character, elapsed:Float)
 {
 	if (isChar(character))
 	{
-		if (character.animation.curAnim.name == 'hairFall' && character.animation.curAnim.finished)
+		if (character.animation.name == 'hairFall' && character.animation.finished)
 		{
 			character.danced = true;
 			character.playAnim('danceRight');
